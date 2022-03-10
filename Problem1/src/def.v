46df176a60ad29edf34bd201926f286192d4bbe6
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-03-10 10:48:55 +0800

    Define the bit pattern for each color

---
+++ Problem1/src/def.v
@@ -0,0 +1,13 @@
+/* filename def.v
+ * Define the bit pattern for each color of RGB leds.
+ */
+`ifndef _DEF_V
+`define _DEF_V
+
+`define RED    3'b100
+`define GREEN  3'b010
+`define BLUE   3'b001
+`define YELLOW 3'b110
+`define WHITE  3'b111
+
+`endif
