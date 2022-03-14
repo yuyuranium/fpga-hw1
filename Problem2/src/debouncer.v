ab3d9669758e2e702d4ee65a8b526dfca2df3f9f
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-03-14 22:23:12 +0800

    Improve coding style in debouncer.v

--- Problem2/src/debouncer.v
+++ Problem2/src/debouncer.v
@@ -9,16 +9,20 @@ module debouncer (
 
   always @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      cnt <= 24'hffffff;
-      debounced_o <= 1'b0;
+      debounced_o <= btn_i;
+      cnt         <= 24'hffffff;
     end else begin
-      if (cnt == 24'hffffff) begin
+      if (&cnt) begin
         if (btn_i != debounced_o) begin
           debounced_o <= btn_i;
-          cnt <= 24'd0;
+          cnt         <= 24'd0;
+        end else begin
+          debounced_o <= debounced_o;
+          cnt         <= cnt;
         end
       end else begin
-        cnt <= cnt + 24'd1;
+        debounced_o <= debounced_o;
+        cnt         <= cnt + 24'd1;
       end
     end
   end
